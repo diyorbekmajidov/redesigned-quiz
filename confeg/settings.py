969de73f40334f858c5650e7b86157a6cc33c112
Diyorbek <majidovdiyorbek741@gmail.com>
2026-02-02 11:17:43 +0500

    database change postgresql

--- confeg/settings.py
+++ confeg/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'confeg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'RedesignedQuiz',
+        'USER': 'RedesignedQuizUser',
+        'PASSWORD': 'QUIZ-USER-312',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
