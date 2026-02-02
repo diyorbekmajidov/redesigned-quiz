e7d2cda11e5fc821b854913b633eab8ad1ce780c
Diyorbek <majidovdiyorbek741@gmail.com>
2026-02-02 11:27:22 +0500

    1

--- confeg/settings.py
+++ confeg/settings.py
@@ -95,14 +95,15 @@ WSGI_APPLICATION = 'confeg.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'RedesignedQuiz',
         'USER': 'RedesignedQuizUser',
         'PASSWORD': 'QUIZ-USER-312',
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
